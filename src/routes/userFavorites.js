0c8535d636d3e1f2a95ac65842a6da1d085d4951
alfredoT7 <123456alfredotorri@gmail.com>
2024-11-05 22:56:37 -0400

    feat:Created endpoint to get Favorites Routes by ID

--- src/routes/userFavorites.js
+++ src/routes/userFavorites.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const userFavoritesController = require('../controllers/userFavoritesController');
 
 router.post('/save', userFavoritesController.createUserFavorites);
+router.get('/:idUser', userFavoritesController.getUserFavorites);
 module.exports = router;
