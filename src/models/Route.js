571cc53c9fcdfd2dd0c921855508c4787c8ad507
alfredoT7 <123456alfredotorri@gmail.com>
2024-11-23 21:43:41 -0400

    feat: routeSearchController done

--- src/models/Route.js
+++ src/models/Route.js
@@ -7,9 +7,24 @@ const routeSchema = new mongoose.Schema({
     unique: true
   },
   geojson: {
-    type: Object,
-    required: true,
+    type: {type:String, default: 'FeatureCollection'},
+    features: [
+      {
+        type: {
+          type: String,
+          required: true,
+          enum: ['Feature']
+        },
+        geometry: {
+          type: { type: String, required: true, enum: ['LineString'] },
+          coordinates: { type: [[Number]], required: true }
+        },
+        properties: Object
+      }
+    ] 
   }
 });
+routeSchema.index({ 'geojson.features.geometry': '2dsphere' });
+
 
 module.exports = mongoose.model('Route', routeSchema);
